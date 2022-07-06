1648bb2c68d0f653a11649ece803806087f1f95a
h360hiro <108774032+h360hiro@users.noreply.github.com>
2022-07-06 12:32:55 +0900

    Update main.c
    myRoot の y は double 型に
    それに伴い myRoot 自体の戻り値の定義も double 型に

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <math.h>
 
-int myRoot(double x) // x の平方根を返す関数。x >= 0 ではないとき exit(1) とする。 
+double myRoot(double x) // x の平方根を返す関数。x >= 0 ではないとき exit(1) とする。 
 {
-  int y;
+  double y;
   if( x <= 0 ){
     exit(1);
   }
