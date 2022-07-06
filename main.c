e7fded3a430b4ea986b314337a40911b90a801b8
h360hiro <108774032+h360hiro@users.noreply.github.com>
2022-07-06 12:29:01 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-int myRoot(double x) // x の平方根を返す関数。x >= 0 ではないとき exit(1) とする。 
+double myRoot(double x) // x の平方根を返す関数。x >= 0 ではないとき exit(1) とする。 
 {
-  int y;
-  if( x <= 0 ){
+  double y;
+  if( x < 0 ){
     exit(1);
   }
   y = sqrt(x);
